ef0a30d4b2a03f71cf2c79de2696fb9f3708fc37
Harith Bennet <82492613+rithbennet@users.noreply.github.com>
2025-09-21 15:53:51 +0800

    I HOPE ITS FIXED

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,6 +6,7 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+    output: 'standalone',
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
